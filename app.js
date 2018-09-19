c612dc05a32a6bc067a56a7abc26913237a2bc71
Nick2510 <nickgh5010@gmail.com>
2018-09-18 23:46:43 -0400

    needs to check answers

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-const score = 0;
-const timer= 60;
-const response= [];
+$(document).ready(function () {
+    var timer = 61;
+    var score = 0;
+    var intervalId;
+    var response = [];
+    var response1;
+    var response2;
+    var response3;
 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-
-  function decrement() {
-      timer--;
-      $('.timer').html('<h2>' + timer + '</h2>')
-      if (timer === 0) {
-          stop();
-          alert("Time Up!");
-      }
-  }
-
-  $('.submit').click(function(){
-    for(i= 0; i > response.length; i++){
-        if(response[i] === 'correct') {
-            score= 0;
-        }
-
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
     }
-  })
 
-  $('.response1').click(function(){
-      response1= this.className;
-  })
+    function decrement() {
+        timer--;
+        $("#timer").html("<h2>" + timer + "</h2>");
+
+        if (timer === 0) {
+            stop();
+            alert("Time Up!");
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    run()
+
+
+    $('.response1').click(function () {
+        response1 = this.className;
+    })
+
+    $('.response2').click(function () {
+        response1 = this.className;
+    })
+
+    $('.submit').click(function () {
+        response.push(response1, response2, response3)
+        for (i = 0; i > response.length; i++) {
+            if (response[i] === 'correct') {
+                score++;
+
+            }
+
+        }
+        $('#timer').hide();
+        $('.question').hide();
+        $("#score").html("<h2> You got " + score + " questions correct</h2>");
+
+    })
+})
